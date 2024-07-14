7dfa395b410eba03c26ecad67cf6a7f9a715f3c8
bodhichristian <clavelle23@gmail.com>
2024-07-14 09:55:44 -0400

    initial dashboard ui

--- Steps/Steps/ContentView.swift
+++ Steps/Steps/ContentView.swift
@@ -9,13 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ScrollView {
+                VStack(spacing: 20) {
+                    
+                    
+                    // Steps Card
+                    VStack(alignment: .leading) {
+                        // Card Header
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                
+                                Text("Avg: 10k Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundStyle(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        // Card Body
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(Color(.secondarySystemBackground))
+                    }
+                    
+                    
+                    // Averages Card
+                    VStack(alignment: .leading) {
+                        // Card Header
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Averages", systemImage: "calendar")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                
+                                Text("Last 28 Days")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            
+                        }
+                        .padding(.bottom, 12)
+                        
+                        // Card Body
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                        
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background {
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(Color(.secondarySystemBackground))
+                    }
+                    
+                }
+                
+            }
+            .navigationTitle("Dashboard")
+            .padding()
         }
-        .padding()
     }
 }
 
