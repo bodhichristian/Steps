9788faca1b3794e01cc9be53a5d21185bbe16911
bodhichristian <clavelle23@gmail.com>
2024-08-14 17:04:09 -0400

    pie chart interactivity

--- Steps/Steps/Charts/StepPieChart.swift
+++ Steps/Steps/Charts/StepPieChart.swift
@@ -9,8 +9,19 @@ import SwiftUI
 import Charts
 
 struct StepPieChart: View {
+    @State private var rawSelectedChartValue: Double?
     
     var chartData: [WeekdayChartData]
+    
+    var selectedWeekday: WeekdayChartData? {
+        guard let rawSelectedChartValue else { return nil }
+
+        var total = 0.0
+        return chartData.first {
+            total += $0.value
+            return rawSelectedChartValue <= total
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading) {
@@ -35,26 +46,42 @@ struct StepPieChart: View {
                     SectorMark(
                         angle: .value("Average Steps", weekday.value),
                         innerRadius: .ratio(0.618),
-                       angularInset: 1
+                        outerRadius: selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 140 : 110,
+                        angularInset: 1
                     )
                     .foregroundStyle(.pink.gradient)
                     .cornerRadius(6)
-                    
-                    // Annotation option. Difficult to visually manage on pie chart.
-//                    .annotation(position: .overlay) {
-//                        Text(weekday.value, format: .number.notation(.compactName))
-//                            .foregroundStyle(.white)
-//                            .fontWeight(.bold)
-//                    }
-                    
- 
-//                    // Create a visual hierarchy based on days of the week
-//                    SectorMark(angle: .value("Average Steps", weekday.value))
-//                        .foregroundStyle(by: .value("Weekday", weekday.date.weekdayTitle))
+                    .opacity(selectedWeekday == nil ? 1.0 : selectedWeekday?.date.weekdayInt == weekday.date.weekdayInt ? 1.0 : 0.3)
                 }
             }
+            .chartAngleSelection(value: $rawSelectedChartValue.animation(.easeInOut))
             .chartLegend(.hidden)
             .frame(height: 240)
+            .chartBackground { proxy in
+                GeometryReader { geo in
+                    if let plotFrame = proxy.plotFrame {
+                        
+                        let frame = geo[plotFrame]
+                        if let selectedWeekday {
+                            VStack(alignment: .center) {
+                                Text(selectedWeekday.date.weekdayTitle)
+                                    .font(.title3.bold())
+                                    .contentTransition(.numericText())
+                                
+                                Text("\(Int(selectedWeekday.value))")
+                                    .fontWeight(.medium)
+                                    .foregroundStyle(.secondary)
+                                    .contentTransition(.numericText())
+                            }
+                            .position(x: frame.midX, y: frame.midY)
+                        } else {
+                            Text("Select a day")
+                                .foregroundStyle(.secondary)
+                                .position(x: frame.midX, y: frame.midY)
+                        }
+                    }
+                }
+            }
         }
         .padding()
         .background {
@@ -67,3 +94,23 @@ struct StepPieChart: View {
 #Preview {
     StepPieChart(chartData: ChartMath.averageWeekdayCount(for: HealthMetric.mockData))
 }
+
+
+
+
+
+
+
+
+
+
+//                    // Create a visual hierarchy based on days of the week
+//                    SectorMark(angle: .value("Average Steps", weekday.value))
+//                        .foregroundStyle(by: .value("Weekday", weekday.date.weekdayTitle))
+
+// Annotation option. Difficult to visually manage on pie chart.
+//                    .annotation(position: .overlay) {
+//                        Text(weekday.value, format: .number.notation(.compactName))
+//                            .foregroundStyle(.white)
+//                            .fontWeight(.bold)
+//                    }
